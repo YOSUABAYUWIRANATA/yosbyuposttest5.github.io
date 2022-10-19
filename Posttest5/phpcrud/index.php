5f9ac5462ca28c070569eef6bef3ff252b4119ea
YOSUA BAYU WIRANATA <113372842+YOSUABAYUWIRANATA@users.noreply.github.com>
2022-10-19 23:35:36 +0800

    Add files via upload

---
+++ Posttest5/phpcrud/index.php
@@ -0,0 +1,14 @@
+<?php
+include 'functions.php';
+
+?>
+
+<?=template_header('Home')?>
+
+<div class="content">
+	<h2>Home</h2>
+	<p>SELAMAT DATANG DI YOSUA CRUD</p>
+	<P> SAYANG</P>
+</div>
+
+<?=template_footer()?>
